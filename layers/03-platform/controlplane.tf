ce13ed13bbb9d1d7c0d70902584ef47ff59aefc6
MASKUN2 <maskunflower2@naver.com>
2026-04-19 15:22:59 +0900

    refactor(infra): remove inline config locals, pass S3 bucket to user_data

--- layers/03-platform/controlplane.tf
+++ layers/03-platform/controlplane.tf
@@ -1,16 +1,3 @@
-locals {
-  # Alloy 및 Docker Compose 설정을 미리 렌더링하여 가독성 개선
-  alloy_config = templatefile("${path.module}/templates/config.alloy.tftpl", {
-    instance_name = "quietchatter-controlplane-node"
-    loki_url      = data.terraform_remote_state.base.outputs.grafana_cloud_logs_url
-    loki_user     = data.terraform_remote_state.base.outputs.grafana_cloud_user
-  })
-
-  docker_compose_config = templatefile("${path.module}/templates/docker-compose.controlplane.yaml.tftpl", {
-    db_username = data.terraform_remote_state.base.outputs.db_username
-  })
-}
-
 # 데이터가 보존되어야 하는 EBS 볼륨 (인스턴스 교체와 독립적)
 resource "aws_ebs_volume" "controlplane_data" {
   # 인스턴스가 위치한 첫 번째 프라이빗 서브넷의 AZ를 동적으로 참조
@@ -48,12 +35,9 @@ resource "aws_instance" "controlplane" {
   user_data_replace_on_change = true
 
   user_data = templatefile("${path.module}/templates/user_data.sh.tftpl", {
-    aws_region                  = var.aws_region
-    db_password_secret_name     = data.terraform_remote_state.base.outputs.db_password_secret_name
-    grafana_api_key_secret_name = data.terraform_remote_state.base.outputs.grafana_api_key_secret_name
-    alloy_config                = local.alloy_config
-    init_db_sql                 = file("${path.module}/init-db.sql")
-    docker_compose_config       = local.docker_compose_config
+    aws_region     = var.aws_region
+    s3_bucket_name = aws_s3_bucket.controlplane_config.bucket
+    init_db_sql    = file("${path.module}/init-db.sql")
   })
 
   # 데이터 유실 방지를 위한 AMI 교체 무시 설정
